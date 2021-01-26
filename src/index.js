b2330eae748d325154a9139cfd1b30bbf1743395
cioddi <maxtobiasweber@gmail.com>
2021-01-26 13:29:54 +0100

    Add correct export for createPdfButton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as MapComponentsProvider } from './components/MapComponentsProvider';
 export { default as MapContext } from './components/MapContext';
 export { default as MapLibreMap } from './components/MapLibreMap';
+export { default as CreatePdfButton } from './components/CreatePdfButton/CreatePdfButton';
