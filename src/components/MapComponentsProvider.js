34f6ede1d6a06280030b5fee3be8dcf5bdf9a2ab
cioddi <maxtobiasweber@gmail.com>
2021-04-14 17:15:17 +0200

    Update MapComponentsProvider comments

--- src/components/MapComponentsProvider.js
+++ src/components/MapComponentsProvider.js
@@ -3,11 +3,10 @@ import PropTypes from "prop-types";
 import { MapContextProvider } from "./MapContext";
 
 /**
- * TODO Update readme for multi instance functionality
- * MapComponentsProvider must be imported and wrapped around component where at least one of its child nodes requires access to the MapLibreMaps object.
+ * MapComponentsProvider must be imported and wrapped around component where at least one of its child nodes requires access to a MapLibre-gl or openlayers instance that is registered in this mapContext.
 MapComponentsProvider must be used one level higher than the first use of MapContext.
  *
- * MapComponentsProvider requires exactly one use of the MapLibreMap component somewhere down the component tree that will create the MapLibre-gl object and set the reference at MapContext.map.
+ * MapComponentsProvider requires at least one use of the MapLibreMap component somewhere down the component tree that will create the MapLibre-gl object and set the reference at MapContext.map. For MapLibre maps it is a good idea to provide a mapId attribute to the MapLibreMap Component even if you are only using a single map instance at start. It will make a later transition to using multiple instances within the same project much easier.
  */
 const MapComponentsProvider = ({ children }) => {
   const [map, setMap] = useState(null);
