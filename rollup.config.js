02362831f6134db0898f3c8a9b3a59ec9877f584
cioddi <maxtobiasweber@gmail.com>
2021-05-03 14:52:59 +0200

    Setup repository build/dev tools to enable the use of typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,24 @@
-import babel from '@rollup/plugin-babel';
-import external from 'rollup-plugin-peer-deps-external';
-import del from 'rollup-plugin-delete';
-import pkg from './package.json';
+import babel from "@rollup/plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+
+import del from "rollup-plugin-delete";
+import pkg from "./package.json";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-    input: pkg.source,
-    output: [
-        { file: pkg.main, format: 'cjs' },
-        { file: pkg.module, format: 'esm' }
-    ],
-    plugins: [
-        external(),
-        babel({
-            exclude: 'node_modules/**'
-        }),
-        del({ targets: ['dist/*'] }),
-    ],
-    external: Object.keys(pkg.peerDependencies || {}),
+  input: pkg.source,
+  output: [{ file: pkg.module, format: "esm", sourcemap:true }],
+  plugins: [
+    typescript({
+      tsconfig: "tsconfig.json",
+      module: "esnext",
+    }),
+    external(),
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+    }),
+    del({ targets: ["dist/*"] }),
+  ],
+  external: ["prop-types","d3", ...Object.keys(pkg.peerDependencies || {})],
 };
